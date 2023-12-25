43d328e02bcf879a4ea52f1b083965bcc38eca1f
Ranidu Harshana <harshanaranidum@gmail.com>
2023-12-26 01:50:25 +0530

    initial commit

---
+++ config-local/src/main/java/com/ranidu/rentacar/configlocal/ConfigLocalApplication.java
@@ -0,0 +1,16 @@
+package com.ranidu.rentacar.configlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigLocalApplication.class, args);
+	}
+
+}
