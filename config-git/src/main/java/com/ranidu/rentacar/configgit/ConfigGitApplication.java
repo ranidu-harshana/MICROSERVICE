c8a19409e77fff4aba93f0694cedb9b977d87a1b
Ranidu Harshana <harshanaranidum@gmail.com>
2023-12-26 01:55:47 +0530

    git conf

---
+++ config-git/src/main/java/com/ranidu/rentacar/configgit/ConfigGitApplication.java
@@ -0,0 +1,13 @@
+package com.ranidu.rentacar.configgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigGitApplication.class, args);
+	}
+
+}
